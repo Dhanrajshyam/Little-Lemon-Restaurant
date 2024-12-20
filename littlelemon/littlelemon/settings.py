673946a0b4749cc47092fc22bdfee4bf960a1915
Dhanrajshyam <shyamjck@gmail.com>
2024-12-20 19:23:24 +0530

    Added initial homepage for the app

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'littlelemonapp.apps.LittlelemonappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
