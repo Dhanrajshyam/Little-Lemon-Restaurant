f3c9ba1f0acb58a3a85bb5756a5d3151c277428d
Dhanrajshyam <shyamjck@gmail.com>
2024-12-21 12:28:06 +0530

    added a contact us view

--- littlelemon/littlelemonapp/views.py
+++ littlelemon/littlelemonapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 def homepage(request):
     return HttpResponse('Home page for little Lemon Restuarant')
+
+
+class ContactusView(TemplateView):
+    template_name = 'contactus.html'
+
+    
