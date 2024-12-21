ac8eb0fb22e5bc74920f5b46f884cdb41847dfa8
Dhanrajshyam <shyamjck@gmail.com>
2024-12-21 12:46:54 +0530

    added contact us base html code in the app.

--- littlelemon/littlelemonapp/views.py
+++ littlelemon/littlelemonapp/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from os import path
 
 # Create your views here.
 def homepage(request):
     return HttpResponse('Home page for little Lemon Restuarant')
 
-
+APP_PATH = path.realpath(path.dirname(__file__))
+TEMPLATE_PATH = path.join(APP_PATH, 'templates/')
 class ContactusView(TemplateView):
     template_name = 'contactus.html'
 
-    
